b43e06eeb2355d0c823994ce777336a08e4322d3
Vish Ganapathy <87033568+evolvedHow@users.noreply.github.com>
2025-03-19 13:25:04 -0400

    Update synthTx.py

--- app/synthTx.py
+++ app/synthTx.py
@@ -7,19 +7,10 @@ import json
 import io
 from google.api_core.client_options import ClientOptions
 
-PROJECT_ID = 'vishretail-2301'
-LOCATION = 'us'
-PROCESSOR_ID ='5ce173ab9343f3d7'
-MIME_TYPE = 'application/pdf'
-PDF_PROCESSOR = 'projects/vishretail-2301/locations/us/processors/5ce173ab9343f3d7'
-API_KEY = 'AIzaSyBrurVe98zATq_4iYl6sfRmn8WvDLFsYYc'
 
-docai_client = documentai.DocumentProcessorServiceClient(
-  client_options=ClientOptions(api_endpoint="us-documentai.googleapis.com"
-  )
-)
 
-RESOURCE_NAME = docai_client.processor_path(PROJECT_ID, LOCATION, PROCESSOR_ID)
+
+
 
 app = FastAPI(
   title='SynthTx,
@@ -29,15 +20,7 @@ app = FastAPI(
 )
 OUTPUT = '/content/drive/MyDrive/1Colab/synthcurve/output/transactions.json'
 
-  transactions = SynthRetailOrders(customer_count=25)
-  tx = transactions.generateTx(order_count=10000)
-  print(f'Got {len(tx)} orders')
-  print(json.dumps(tx, indent=2))
-
-  with open(OUTPUT, 'w') as f:
-    for row in tx:
-      f.write(json.dumps(row) + '\n')
-
+  
 @app.get("/")
 async def index():
   return("Hello world!")
@@ -59,39 +42,16 @@ async def generate(order_count=1000,
                 format="JSON",
 ):
 
+  transactions = SynthRetailOrders()
+  tx = transactions.generateTx(order_count=10000)
+  print(f'Got {len(tx)} orders')
+  print(json.dumps(tx, indent=2))
+
+  with open(OUTPUT, 'w') as f:
+    for row in tx:
+      f.write(json.dumps(row) + '\n')
+return(f'Output is {tx}'
   
-  return("generation wip")
-
-@app.post(
-  '/v1/parsePDF/',
-  summary='Parses a PDF using the DocAI api',
-  description='This API uses the DocAI API to perform multi-modal parsing of a document.',
-)
-async def parsePDF(pdfFile:UploadFile, chunk_size: int, chunk_overlap: int, chunk_uom:str='char'):
-
-  if pdfFile.filename.endswith('pdf'):
-    pass
-  else:
-    return('error')
-
-  pdf_bytes = await pdfFile.read()
-
-  raw_document = documentai.RawDocument(content=pdf_bytes, mime_type=MIME_TYPE)
-
-  request = documentai.ProcessRequest(name=RESOURCE_NAME, raw_document=raw_document)
-  response = docai_client.process_document(request=request)
-
-  text = ""
-  for page in response.document.pages:
-    for block in page.blocks:
-      for paragraph in block.paragraphs:
-        for word in paragraph.words:
-          text += word.symbol
-          if word.break_category == documentai.DocumentProcessor.EntityBreakCategory.SPACE:
-            text += ""
-  return text
-
-  return("wip")
 
 if __name__ == "__main__":
   uvicorn.run("gretail:app", host="0.0.0.0", reload=True,   port=8080)
